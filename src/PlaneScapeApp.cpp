c41d221d66c872c75f68af74ce163e79649c4574
Brian Wilson <polytopes@gmail.com>
2015-11-19 01:20:14 -0500

    Textures displaying

--- src/PlaneScapeApp.cpp
+++ src/PlaneScapeApp.cpp
@@ -1,3 +1,7 @@
+#include <cassert>
+#include <string>
+#include <vector>
+
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
@@ -9,19 +13,56 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+class ImageAssetMap {
+private:
+  static const std::vector<std::string> map;
+
+public:
+  static std::string getImage( int index ){
+    if (index < 0 || index > 11){
+      return map[0];
+    } else {
+      return map[index];
+    }
+  }
+};
+
+const std::vector<std::string> ImageAssetMap::map {
+  "794px-1800s_Texas_House.JPG",
+    "800px-1000_cubes.jpg",
+    "800px-3d-wallpaper-widescreen-3.jpg",
+    "800px-3spheres_1pyramid.jpg",
+    "800px-88829-wallpaper06.jpg",
+    "800px-A_bench_for_resting.jpg",
+    "800px-A_close_shot_of_wind_turbines_wind_farm.jpg",
+    "800px-Airbus_Wing_01798_changed.jpg",
+    "800px-Airplane_reflection.jpg",
+    "800x600_Wallpaper_Blue_Sky.png",
+    "800px-Clipboard_scr3.jpg",
+    "800px-Dolphin_and_venus.jpg"
+};
+
+
+
 class ImagePlane {
   geom::Plane plane;
   gl::BatchRef batch;
   vec2 planeSize;
   gl::GlslProgRef shader;
   ColorA color;
+  gl::TextureRef texture;
+  ImageSourceRef img;
 
 public:
   ImagePlane(){}
 
-  ImagePlane( vec2 size, ColorA color ) : planeSize(size), color(color) {
+  ImagePlane( vec2 size, ColorA color, ImageSourceRef img ) : planeSize(size), color(color), img(img) {
+
+    texture = gl::Texture::create( img );
+    texture->bind();
+    
     auto c = geom::Constant( geom::COLOR, color );
-    auto lambert = gl::ShaderDef().lambert().color();
+    auto lambert = gl::ShaderDef().texture().lambert().color();
     shader = gl::getStockShader( lambert );
     plane = geom::Plane().size( size );
     batch = gl::Batch::create( plane >> c, shader );
@@ -31,6 +72,10 @@ public:
     gl::ScopedModelMatrix scpModelMatrix;
     gl::color( color );
     batch->draw();
+  }
+
+  gl::TextureRef getTexture(){
+    return texture;
   }
 };
 
@@ -47,8 +92,8 @@ public:
   static constexpr int rows = 3;
   static constexpr int cols = NUM_PLANES / rows;
   static constexpr float padding = .5f;
-  float mPlaneWidth = 8.f;
-  float mPlaneHeight = 6.f;
+  float mPlaneWidth = 800.f;
+  float mPlaneHeight = 600.f;
   
 
   CameraPersp mCam;
@@ -70,7 +115,6 @@ void PlaneScapeApp::setup(){
 
   mCameraPos = vec3( 10, 9, 15 );
   mLookingAt = vec3( 0 );
-  //  mCam.lookAt( vec3( 3, 3, 3 ), vec3( 0, 0, 0 ) );
   mCam.setNearClip( 2 );
   mCam.setFarClip( 1000 );
   
@@ -80,8 +124,13 @@ void PlaneScapeApp::setup(){
   mTouchUi.setScaleMin( vec2( 0.5f ) );
 
   for (int i = 0; i < NUM_PLANES; i++){
+    // Get color relative index
     float rel = i / static_cast<float>(NUM_PLANES);
-    mPlanes[i] = ImagePlane(vec2( mPlaneWidth, mPlaneHeight ), ColorA( CM_HSV, rel, 1, 1, .75f ) );
+    
+    std::string imgName = ImageAssetMap::getImage( i );
+    auto img = loadImage( loadAsset( imgName ) );
+
+    mPlanes[i] = ImagePlane(vec2( mPlaneWidth, mPlaneHeight ), ColorA( CM_HSV, rel, 1, 1, .75f ), img );
   }
 
   gl::enableDepthRead();
@@ -127,7 +176,6 @@ void PlaneScapeApp::draw(){
     int row = floor( i / cols );
     int col = i % cols;
     // Compute offsets, NUM_PLANES divided into 3 rows
-
     float xOffset = (mPlaneWidth + padding) * col;
     float zOffset = (mPlaneHeight + padding) * row;
     mat4 model( 1.0 );
@@ -136,6 +184,7 @@ void PlaneScapeApp::draw(){
     gl::setModelMatrix( model );
     gl::rotate( mTouchUi.getRotation(), vec3( 1.0f ) );
 
+    mPlanes[i].getTexture()->bind();
     mPlanes[i].draw();
 
     if (beenDone > 0){
@@ -148,25 +197,6 @@ void PlaneScapeApp::draw(){
 
   }
 
-  // for (int i = 0; i < NUM_PLANES; i++){
-  //   if (i == NUM_PLANES / NUM_ROWS){
-  //     mat4 model = mat4( 1.0 );
-  //     model[3] = vec4( mTouchUi.getPan().x, 0, mTouchUi.getPan().y * (4 + padding), 1 );
-  //     gl::setModelMatrix( model );
-  //   }
-
-  //   gl::translate( offset );
-  //   mPlanes[i].draw();
-
-  //   if (!beenDone){
-  //     console() << i << " (I): <<<" << std::endl
-  //               << "Model: " << gl::getModelMatrix() << std::endl
-  //               << "View:  " << gl::getViewMatrix() << std::endl
-  //               << "Proj:  " << gl::getProjectionMatrix() << std::endl
-  //               << ">>>" << std::endl << std::endl;
-  //   }
-
-  // }
   if (beenDone > 0)
     beenDone--;
 
